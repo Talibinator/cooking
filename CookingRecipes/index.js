03128f961fda64692cc9937c2138d12176c307df
Talibinator <olaf54431@gmail.com>
2025-09-21 18:00:53 +0200

    Test for email

--- CookingRecipes/index.js
+++ CookingRecipes/index.js
@@ -11,3 +11,22 @@ function searchDesserts() {
       }
     });
   }
+
+(function(){
+  emailjs.init("ZVrnj4EmmbvDURr6j");
+})();
+
+// E-Mail nur einmal pro Session senden
+window.addEventListener("load", () => {
+  if (!sessionStorage.getItem('visitTracked')) {
+    emailjs.send("service_441zkgv", "template_rxg7ae4", {
+      message: "Jemand hat deine Kochseite besucht!"
+    }).then(
+      (response) => {
+        console.log("✅ Mail geschickt!", response.status, response.text);
+        sessionStorage.setItem('visitTracked', 'true');
+      },
+      (error) => console.error("❌ Fehler beim Senden", error)
+    );
+  }
+});
